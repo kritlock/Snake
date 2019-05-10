06ecee20d6259d7de388e286ee782f4fbe6ac2e7
kritlock <kritlock@yandex.ru>
2019-05-10 19:36:28 +0300

    + list

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,7 +12,11 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 4, '*');
+            Point p2 = new Point(6, 8, '*');
             p1.Draw();
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
         }
     }
 }
