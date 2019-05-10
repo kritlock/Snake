bed843a7ba0e2917ca5d34dbfc4b0833d1340fb1
kritlock <kritlock@yandex.ru>
2019-05-10 18:40:49 +0300

    drow + randomizer

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,9 +8,23 @@ namespace Snake
 {
     class Program
     {
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("хуй");
+            Random r = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int a = r.Next(50);
+                int b = r.Next(50);
+                char c = ('*');
+                Draw(a, b, c);
+            }
+
         }
     }
 }
